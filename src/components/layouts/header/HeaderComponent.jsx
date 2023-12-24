2aedf75567bd49296446e469f22c9638aa43c631
alaminpu1007 <alamin66.sit@gmail.com>
2023-12-24 20:01:21 +0600

    initialized project with necessary configuration

---
+++ src/components/layouts/header/HeaderComponent.jsx
@@ -0,0 +1,34 @@
+'use client';
+
+import Link from 'next/link';
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+
+const HeaderComponent = () => {
+    return (
+        <div id='navbar' className=''>
+            <Navbar bg='dark' data-bs-theme='dark'>
+                <div className='container'>
+                    <Navbar.Brand as={Link} href='/'>
+                        Navbar
+                    </Navbar.Brand>
+                    <Nav className='me-auto'>
+                        <Nav.Link as={Link} href='/dashboard'>
+                            dashboard
+                        </Nav.Link>
+                        <Nav.Link as={Link} href='#features'>
+                            Features
+                        </Nav.Link>
+                        <Nav.Link as={Link} href='#pricing'>
+                            Pricing
+                        </Nav.Link>
+                    </Nav>
+                </div>
+            </Navbar>
+        </div>
+    );
+};
+
+export default HeaderComponent;
